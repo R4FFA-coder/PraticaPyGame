1c02030aff46aa30293f8041dc3c93062df01b76
Rafa <rafinhazero12@gmail.com>
2026-02-13 09:02:26 -0300

    Revert "Ajuste no esboço do Primeiro Codigo para o jogo"
    This reverts commit 9b6361489cc2174cc14b563464269c616bcc80b6.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pygame.locals import *
 from sys import exit
 
 pygame.init()
-clock = pygame.time.Clock()
 
 width = 1280
 height = 720
@@ -16,8 +15,4 @@ while True:
         if event.type == QUIT:
             pygame.quit()  # Close Window
             exit()  # Quit Pygame
-        pygame.draw.rect(window, (0, 101, 255), (100, 360, 40, 50))
-        pygame.draw.circle(window, (255, 0, 0), (640, 200), 100)
-        pygame.draw.line(window, (255, 255, 255), (540, 200), (120, 360), 5)
     pygame.display.update()
-    clock.tick(60)
